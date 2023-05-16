ba4426cb99244f9c676fff04f3941f6309515a4d
Samcolserra <samcolserra@gmail.com>
2023-05-16 15:36:10 +0300

    Added boilerplate for routing and pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import Profile from './pages/Profile'
+import Cart from './pages/Cart'
+import Product from './pages/Product'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/product",
+    element: <Product />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+    errorElement: <NotFound />,
+  }
+])
 
 const App = () => {
   return (
-    <div>Hello world!</div>
+    <RouterProvider router={router} />
   )
 }
 
